593485edd0ef8878e63005adff440cbdb52bbc54
Malik El Guermai <malikelguermai@outlook.com>
2022-03-03 18:18:18 -0500

    first commit

---
+++ MovieDatabase_Lab/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase_Lab
+{
+    public class Movie
+    {
+        //Inputs go here
+        private string _title { get; set; }
+        private string _category { get; set; }
+
+        //Constructor
+        public Movie(string t, string c)
+        {
+            _title = t;
+            _category = c;
+        }
+
+        //Method for category
+
+        public string GetCategory()
+        {
+            return _category;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+    }
+}
---
+++ MovieDatabase_Lab/Program.cs
@@ -0,0 +1,51 @@
+﻿using MovieDatabase_Lab;
+
+//Display correct movie
+var value = true;
+do
+{
+    var moviesList = new List<Movie>
+            {
+                new Movie("Encanto", "animated"),
+                new Movie("The Proud Family","animated"),
+                new Movie("The GodFather",  "drama"),
+                new Movie("A Bronx Tale", "drama"),
+                new Movie("Get Out",  "horror"),
+                new Movie("It", "horror"),
+                new Movie("Black Panther", "scifi"),
+                new Movie("The Wizard Of Oz", "scifi"),
+                new Movie("Taken", "drama" ),
+                new Movie("Mr. Brooks",  "drama"),
+            };
+
+    //input readline
+    Console.WriteLine("Welcome to the Movie List Application!There are 10 movies in this list. What category are you interested in?");
+    Console.WriteLine("Please choose between 'animated', 'drama', 'horror' and 'scifi'.");
+    var categoryMovie = Console.ReadLine();
+    if (categoryMovie != "animated" && categoryMovie != "drama" && categoryMovie != "horror" && categoryMovie != "scifi")
+    {
+        Console.WriteLine("Please choose one of the categories listed above");
+        return;
+    }
+    else
+    {
+        var animatedMovies = moviesList.Where(x => x.GetCategory() == categoryMovie);
+        foreach (var t in animatedMovies)
+        {
+            Console.WriteLine(t.GetTitle());
+        }
+    }
+    Console.WriteLine("Would you like to continue? y/n");
+    var answer = Console.ReadLine();
+
+    if (answer == "y")
+    {
+        continue;
+    }
+    else
+    {
+        break;
+    }
+}
+while (value);
+
